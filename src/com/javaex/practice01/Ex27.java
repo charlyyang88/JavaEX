6029983ff7d15774a4c8349b9cb657bb7d53fab6
양창재 <cjyang.cjy@gmail.com>
2024-03-15 18:36:04 +0900

    push 확인

--- src/com/javaex/practice01/Ex27.java
+++ src/com/javaex/practice01/Ex27.java
@@ -3,7 +3,7 @@ package com.javaex.practice01;
 import java.util.Scanner;
 
 public class Ex27 {
-// 반지름을 입력 받아 원의 면적을 구하는 프로그램을 작성하세요 파이 값은 3.143.14로 상수로 지정
+// 반지름을 입력 받아 원의 면적을 구하는 프로그램을 작성하세요 파이 값은 3.14로 상수로 지정
 	public static void main(String[] args) {
 		final double pai = 3.14;
 		double r;
