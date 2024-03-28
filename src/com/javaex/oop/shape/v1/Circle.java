ba0df55f151ae53cf7a211a5702ced667e601e6c
양창재 <cjyang.cjy@gmail.com>
2024-03-28 18:24:55 +0900

    240328

---
+++ src/com/javaex/oop/shape/v1/Circle.java
@@ -0,0 +1,26 @@
+package com.javaex.oop.shape.v1;
+
+public class Circle extends Shape {
+	// 필드
+	private double redius;
+	
+	// 생성자
+	public Circle(int x, int y, double redius) {
+		// 부모 생성자 호출
+		super(x, y);
+		this.redius = redius;
+	}
+	
+	// 부모가 선언한 추상메서드는 반드시 구현해야 한다
+	@Override
+	public double area() {
+		double area = Math.PI * Math.pow(redius, 2);
+		return 0;
+	}
+	@Override
+	public void draw() {
+		System.out.printf("원[x=%d, y=%d, r=%f, area=%f]을 그렸어요.%n",
+				x, y, redius, area());
+	}
+
+}
